f5a9a86d363953874f2e9af1cef8aa4b1fcdc739
avetyan-gayane <avetyan.gayane.01@gmail.com>
2023-07-07 17:46:41 +0400

    strings tasks

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,25 +1,56 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodParser {
 
     /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
      * signatureString is a java-like method signature with following parts:
-     *      1. access modifier - optional, followed by space: ' '
-     *      2. return type - followed by space: ' '
-     *      3. method name
-     *      4. arguments - surrounded with braces: '()' and separated by commas: ','
+     * 1. access modifier - optional, followed by space: ' '
+     * 2. return type - followed by space: ' '
+     * 3. method name
+     * 4. arguments - surrounded with braces: '()' and separated by commas: ','
      * Each argument consists of argument type and argument name, separated by space: ' '.
      * Examples:
-     *      accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
-     *      private void log(String value)
-     *      Vector3 distort(int x, int y, int z, float magnitude)
-     *      public DateTime getCurrentDateTime()
+     * accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
+     * private void log(String value)
+     * Vector3 distort(int x, int y, int z, float magnitude)
+     * public DateTime getCurrentDateTime()
      *
      * @param signatureString source string to parse
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        String[] splited = signatureString.split("[ (,)]");
+        List<String> list = new ArrayList<>();
+
+        if (!splited[0].equals("private") && !splited[0].equals("public") && !splited[0].equals("protected"))
+            list.add(null);
+
+        for (String el : splited) {
+            if (!el.isEmpty())
+                list.add(el);
+        }
+
+        String accessModifier = list.get(0);
+        String returnType = list.get(1);
+        String methodName = list.get(2);
+
+        List<MethodSignature.Argument> arguments = new ArrayList<>();
+
+        for (int i = 3; i < list.size() - 1; i += 2) {
+            MethodSignature.Argument argument =
+                    new MethodSignature.Argument(list.get(i), list.get(i + 1));
+            arguments.add(argument);
+        }
+
+        MethodSignature methodSignature = new MethodSignature(methodName, arguments);
+        methodSignature.setAccessModifier(accessModifier);
+        methodSignature.setReturnType(returnType);
+
+        return methodSignature;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,5 +1,7 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -8,11 +10,19 @@ public class StringSplitter {
     /**
      * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        String[] split = source.split(delimiters.toString());
+        List<String> substrings = new ArrayList<>();
+
+        for (String substring : split) {
+            if (!substring.isEmpty())
+                substrings.add(substring);
+        }
+
+        return substrings;
     }
 }
